c9854d89ce861d990a82ab8a5e0fc2f93606b6d1
ghanshyam shukla <gsgmca682@gmail.com>
2024-10-13 15:01:58 +0530

    initial commit-jenkin code

---
+++ demo-project-jenkin-tutorial/src/main/java/com/tutorial/jenkin/User.java
@@ -0,0 +1,42 @@
+package com.tutorial.jenkin;
+
+public class User {
+    public String name;
+    public String first;
+    public String address;
+
+    public String getAge() {
+        return age;
+
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String age;
+}
